86848b05b8b4bf1292a814fde7fd78628583a6a6
CindyGB <49107051+cindyemiranda@users.noreply.github.com>
2022-05-15 16:24:44 -0500

    Update new-element.jsx

--- react/new-element.jsx
+++ react/new-element.jsx
@@ -1,1 +1,36 @@
 const JSX = <h1>Hello JSX!</h1>;
+//Create a Complex JSX Element
+const JSX1=<div>
+  <h1>This is the title</h1>
+  <p>This is the paragraph.</p>
+  <ul>
+    <li>Element 1</li>
+    <li>Element 2</li>
+    <li>Element 3</li>
+  </ul>
+</div>;
+//Add Comments in JSX
+const JSX2 = (
+    <div>
+      {/*This is a code in react. */}
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+    </div>
+  );
+
+//Render HTML Elements to the DOM
+const JSX3 = (
+    <div>
+      <h1>Hello World</h1>
+      <p>Lets render this to the DOM</p>
+    </div>
+  );
+  // Change code below this line
+  ReactDOM.render(JSX3, document.getElementById('challenge-node'));
+
+ //Define an HTML Class in JSX
+ const JSX4 = (
+    <div className="myDiv">
+      <h1>Add a class to this div</h1>
+    </div>
+  );
