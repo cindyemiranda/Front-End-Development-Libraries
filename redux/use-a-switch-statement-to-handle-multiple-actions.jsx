ea0afa287bd38d83cd28de1063b2648f058c5c66
CindyGB <49107051+cindyemiranda@users.noreply.github.com>
2022-06-04 18:15:42 -0500

    Update use-a-switch-statement-to-handle-multiple-actions.jsx

--- redux/use-a-switch-statement-to-handle-multiple-actions.jsx
+++ redux/use-a-switch-statement-to-handle-multiple-actions.jsx
@@ -1,35 +1,39 @@
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
 const defaultState = {
-    authenticated: false
-  };
-  
-  const authReducer = (state = defaultState, action) => {
-    // Change code below this line
-    var newState={};
-     switch (action.type){
-       case 'LOGIN':
-        newState={authenticated: true};  
-        break;
-      case 'LOGOUT':
-        newState={authenticated: false};
-        break;
-      default:
-        newState=state;
-        break;
-     } 
-     return newState;
-    // Change code above this line
-  };
-  
-  const store = Redux.createStore(authReducer);
-  
-  const loginUser = () => {
-    return {
-      type: 'LOGIN'
-    }
-  };
-  
-  const logoutUser = () => {
-    return {
-      type: 'LOGOUT'
-    }
-  };
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+
+  switch (action.type) {
+    case LOGIN: 
+      return {
+        authenticated: true
+      }
+    case LOGOUT: 
+      return {
+        authenticated: false
+      }
+
+    default:
+      return state;
+
+  }
+
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: LOGIN
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: LOGOUT
+  }
+};
